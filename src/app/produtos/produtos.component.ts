f1552f8278c3762e23f6a6d458cf73e7ee0f68f0
elicadv <elicavb@gmail.com>
2023-04-10 15:25:59 -0300

    first commit

---
+++ src/app/produtos/produtos.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { IProduto} from '../produtos';
+import { ProdutosService } from '../produtos.service';
+
+@Component({
+  selector: 'app-produtos',
+  templateUrl: './produtos.component.html',
+  styleUrls: ['./produtos.component.css']
+})
+export class ProdutosComponent {
+
+  produtos: IProduto[] |undefined;
+
+  constructor(
+    private produtoService: ProdutosService,
+    private route: ActivatedRoute
+  ){}
+  ngOnInit(): void{
+    const produtos = this.produtoService.getAll()
+    this.route.queryParamMap.subscribe(params => {
+    const descricao = params.get("descricao")?.toLowerCase();
+    if(descricao){
+      
+   this.produtos = produtos.filter(produto => produto.descricao.toLowerCase().includes(descricao));
+   return;
+    }
+    this.produtos = produtos;
+    })
+  }
+}
